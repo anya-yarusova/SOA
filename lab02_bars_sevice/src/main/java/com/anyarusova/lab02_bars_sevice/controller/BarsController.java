7065e81d279422b2965a8dc44348e43eec6de1de
Anna Yarusova <yarusova.anny@yandex.ru>
2024-11-07 14:54:12 +0300

    add request validation

--- lab02_bars_sevice/src/main/java/com/anyarusova/lab02_bars_sevice/controller/BarsController.java
+++ lab02_bars_sevice/src/main/java/com/anyarusova/lab02_bars_sevice/controller/BarsController.java
@@ -19,6 +19,7 @@ public class BarsController {
     @POST
     @Path("/labwork/{labwork-id}/difficulty/increase/{steps-count}")
     public Response increaseDifficulty(@PathParam("labwork-id") long id, @PathParam("steps-count") int stepsCount) throws ExtendedException {
+        validate(id, stepsCount);
         LabWorkData labWork = barsService.increaseDifficulty(id, stepsCount);
         return Response.ok(labWork).build();
     }
@@ -26,7 +27,14 @@ public class BarsController {
     @POST
     @Path("/labwork/{labwork-id}/difficulty/decrease/{steps-count}")
     public Response decreaseDifficulty(@PathParam("labwork-id") long id, @PathParam("steps-count") int stepsCount) throws ExtendedException {
+        validate(id, stepsCount);
         LabWorkData labWork = barsService.decreaseDifficulty(id, stepsCount);
         return Response.ok(labWork).build();
+    }
+
+    private void validate(long id, int stepsCount) throws ExtendedException {
+        if (id < 0 || stepsCount < 0) {
+            throw new ExtendedException(Response.Status.BAD_REQUEST, "Некорректный ID лабораторной работы или значение для количества шагов");
+        }
     }
 }
